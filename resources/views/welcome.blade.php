014ae786e8cc62f01ed4ca4afc30c41fe247a254
asfandino-dev <asfandino@outlook.com>
2024-10-15 18:47:50 -0500

    Modificacion vista welcome, titulo cursos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
 
     <section class="mt-24">
-        <h2 class="text-center text-3xl text-gray-600">Ultimos cursos!</h2>
+        <h2 class="text-center text-3xl text-gray-600">Ultimas capacitaciones!</h2>
         <p class="text-center text-gray-500 text-sm mb-6">Aqui encontraras nuestros ultimos cursos!</p>
 
         <div class=" mb-2.5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-8 ">
